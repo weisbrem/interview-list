c0747cab235419c158d0ec92be119dd1914e5a06
weisbrem <zhernovkov_konst@mail.ru>
2024-05-19 00:40:23 +0200

    feat: add menu component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <app-header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
