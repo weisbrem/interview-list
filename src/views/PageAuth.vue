00da62147b8b31e835c7d94426f88682e7ab3683
weisbrem <zhernovkov_konst@mail.ru>
2024-05-21 00:30:59 +0200

    feat: create page auth

--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,3 +1,102 @@
+<script setup lang="ts">
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useToast } from 'primevue/usetoast'
+
+const isLogin = ref<boolean>(true)
+const isLoading = ref<boolean>(false)
+
+const email = ref<string>('')
+const password = ref<string>('')
+
+const router = useRouter()
+const toast = useToast()
+
+const toggleAuth = () => {
+  isLogin.value = !isLogin.value
+}
+
+const subtitleText = computed<string>(() => {
+  return isLogin.value ? 'Создайте учетную запись' : 'Войдите в свою учетную запись'
+})
+
+const linkAccountText = computed<string>(() => {
+  return isLogin.value ? 'Нет учетной записи?' : 'Уже есть учетная запись?'
+})
+
+const submitButtonText = computed<string>(() => {
+  return isLogin.value ? 'Вход' : 'Регистрация'
+})
+
+const signUp = async () => {
+  isLoading.value = true
+
+  try {
+    await createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+
+    router.push({ name: 'Home' })
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.add({ severity: 'error', summary: 'Error', detail: error.message, life: 3000 })
+    }
+  } finally {
+    isLoading.value = false
+  }
+}
+
+const signIn = async () => {
+  isLoading.value = true
+
+  try {
+    await signInWithEmailAndPassword(getAuth(), email.value, password.value)
+
+    router.push({ name: 'Home' })
+  } catch (error) {
+    if (error instanceof Error) {
+      toast.add({ severity: 'error', summary: 'Error', detail: error.message, life: 3000 })
+    }
+  } finally {
+    isLoading.value = false
+  }
+}
+
+const submitForm = () => {
+  if (isLogin.value) {
+    signIn()
+  } else {
+    signUp()
+  }
+}
+</script>
+
 <template>
-  <p>page auth</p>
+  <app-toast position="bottom-right" />
+  <div class="flex justify-content-center p-2">
+    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+      <div class="text-center mb-3">
+        <div class="text-900 text-3xl font-medium mb-3">Приветствую!</div>
+        <span class="text-600 font-medium line-height-3">{{ subtitleText }}</span>
+        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="toggleAuth">
+          {{ linkAccountText }}
+        </a>
+      </div>
+
+      <form @submit.prevent="submitForm">
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <app-input-text v-model="email" id="email1" type="email" class="w-full mb-3" />
+
+        <label for="password1" class="block text-900 font-medium mb-2">Пароль</label>
+        <app-input-text v-model="password" id="password1" type="password" class="w-full mb-3" />
+
+        <app-button
+          :label="submitButtonText"
+          type="submit"
+          icon="pi pi-user"
+          :loading="isLoading"
+          class="w-full"
+        ></app-button>
+      </form>
+    </div>
+  </div>
 </template>
