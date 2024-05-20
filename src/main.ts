625e62e2e3d86f538b5543cd9d4d06fa9c49bf2a
weisbrem <zhernovkov_konst@mail.ru>
2024-05-20 23:48:57 +0200

    feat: add firebase config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
+import { initializeApp } from 'firebase/app'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
@@ -10,6 +12,17 @@ import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgy_fnnVn-VX-6jz_i3d2uabDXlNiXnkw',
+  authDomain: 'interviews-vue-app.firebaseapp.com',
+  projectId: 'interviews-vue-app',
+  storageBucket: 'interviews-vue-app.appspot.com',
+  messagingSenderId: '809483682696',
+  appId: '1:809483682696:web:de3d21c84d761e1d39b03d'
+}
+
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
