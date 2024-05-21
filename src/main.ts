c80f3992bf5dcf580681547c21cca06c91e9fc79
weisbrem <zhernovkov_konst@mail.ru>
2024-05-21 23:38:12 +0200

    feat: register new components

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,19 @@ import { initializeApp } from 'firebase/app'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+
 import Menubar from 'primevue/menubar'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
-import ToastService from 'primevue/toastservice'
 import Toast from 'primevue/toast'
 import ProgressSpinner from 'primevue/progressspinner'
 import Card from 'primevue/card'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ConfirmDialog from 'primevue/confirmdialog'
+import InlineMessage from 'primevue/inlinemessage'
 
 import App from './App.vue'
 import router from './router'
@@ -33,6 +39,7 @@ initializeApp(firebaseConfig)
 const app = createApp(App)
 
 app.use(ToastService)
+app.use(ConfirmationService)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
@@ -43,5 +50,9 @@ app.component('app-input-text', InputText)
 app.component('app-toast', Toast)
 app.component('app-progress-spinner', ProgressSpinner)
 app.component('app-card', Card)
+app.component('app-data-table', DataTable)
+app.component('app-column', Column)
+app.component('app-confirm-dialog', ConfirmDialog)
+app.component('app-inline-message', InlineMessage)
 
 app.mount('#app')
