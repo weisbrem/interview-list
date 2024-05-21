cc2d98493f622d4f6c27dc34c7bc7548c5a9187f
weisbrem <zhernovkov_konst@mail.ru>
2024-05-21 03:14:31 +0200

    feat: add interview interface

---
+++ src/types/interview.interface.ts
@@ -0,0 +1,10 @@
+export interface IInterview {
+  id: string
+  createdAt: Date
+  company: string
+  vacancyLink: string
+  hrName: string
+  contactTelegram?: string
+  contactWhatsApp?: string
+  contactPhone?: string
+}
